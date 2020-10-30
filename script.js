d283cc54c2af294ec0f0419f2e961c20c69baaa8
Nick De Marco <nickdmrco@gmail.com>
2020-10-30 14:55:48 -0700

    Final commit

--- script.js
+++ script.js
@@ -1,17 +1,52 @@
 
-function generate() {
- let complexity = document.getElementById('').value
+var generateBtn = document.querySelector("#generate");
 
- //PASSWORD VALUES
- let values = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+'
 
- let password = ''
+function generatePassword() {
+ let length = parseInt(prompt('Choose a password length'))
+ let upper = confirm('want upper?')
+ let lower = confirm('lower')
+ let nums = confirm('nums')
+ let special = confirm('special')
+ let upperChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+ let lowerChar = 'abcdefghijklmnopqrstuvwxyz'
+ let numsChar = '1234567890'
+ let specialChar = '!@#$%^&*()_+'
 
- //creat loop for characters
- for (var i = 0; i <= complexity; i++) {
-  password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)))
+ if (length < 129 && length > 7) {
+  let newPass = ''
+  let newPassChar = ''
+  if (upper) {
+   newPassChar += upperChar
+  }
+  if (lower) {
+   newPassChar += lowerChar
+  }
+  if (nums) {
+   newPassChar += numsChar
+  }
+  if (special) {
+   newPassChar += specialChar
+  }
+  console.log(newPassChar)
+
+  for (let i = 0; i < length; i++) {
+   let newLetter = newPassChar[Math.floor(Math.random() * newPassChar.length)]
+   newPass += newChar
+  }
+  console.log(newPass)
+ } else {
+  alert('wrong length')
  }
 
- //add to textbox
- document.getElementById('#password')value = password
+}// Write password to the #password input
+function writePassword() {
+ var password = generatePassword();
+ var passwordText = document.querySelector("#password");
+
+ passwordText.value = password;
+
 }
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
