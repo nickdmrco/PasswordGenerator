6ada5773e61bc7722ac41d2d39e6f5340ac62c47
nickdmrco <70681753+nickdmrco@users.noreply.github.com>
2020-11-02 21:52:06 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
  let length = parseInt(prompt('Choose a password length'))
- let upper = confirm('want upper?')
- let lower = confirm('lower')
- let nums = confirm('nums')
- let special = confirm('special')
+ let upper = confirm('Would you like uppercase letters?')
+ let lower = confirm('Would you like lowercase letters?')
+ let nums = confirm('Would you like numbers?')
+ let special = confirm('Would you like special characters?')
  let upperChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
  let lowerChar = 'abcdefghijklmnopqrstuvwxyz'
  let numsChar = '1234567890'
@@ -16,16 +16,16 @@ function generatePassword() {
  if (length < 129 && length > 7) {
   let newPass = ''
   let newPassChar = ''
-  if (upper) {
+  if upperChar {
    newPassChar += upperChar
   }
-  if (lower) {
+  if lowerChar {
    newPassChar += lowerChar
   }
-  if (nums) {
+  if numsChar {
    newPassChar += numsChar
   }
-  if (special) {
+  if specialChar {
    newPassChar += specialChar
   }
   console.log(newPassChar)
@@ -39,7 +39,8 @@ function generatePassword() {
   alert('wrong length')
  }
 
-}// Write password to the #password input
+}
+// Write password to the #password input
 function writePassword() {
  var password = generatePassword();
  var passwordText = document.querySelector("#password");
